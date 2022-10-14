a7c0802ee4a2dc3859587aba0477390b326a16ca
Wang <80409907+wanggz1000@users.noreply.github.com>
2022-10-14 17:03:53 +0200

    Initial commit

---
+++ src/main/java/com/example/wordcount/WordCountApplication.java
@@ -0,0 +1,13 @@
+package com.example.wordcount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordCountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordCountApplication.class, args);
+    }
+
+}
